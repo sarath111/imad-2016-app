ada997bb9c4bafa0aebac5859b88a6e6807c1ad3
sarath111 <sarathmohan111@gmail.com>
2016-10-23 11:42:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 alert("Hello");
 var txtfield=document.getElementById("id1");
 txtfield.innerHTML="Hello";
+//move the img element
+var img=document.getElementById('img1');
+img.onclick= function() {
+  alert("clicked on img");
+};

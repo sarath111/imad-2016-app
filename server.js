d2b90da77bcc6c4f4e96acb2e0a453e3ba97dddf
sarath111 <sarathmohan111@gmail.com>
2016-10-30 12:25:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleThree = {
+    title : "Article Three",
+    heading : 'Article Three by Sarath',
+    date : '30-OCT-2016',
+    content : 
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });

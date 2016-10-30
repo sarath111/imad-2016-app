05ee1fa510440d45efb550a249d14b472536c2d1
sarath111 <sarathmohan111@gmail.com>
2016-10-30 13:57:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,5 +43,10 @@ button.onclick = function () {
 var nameText=document.getElementById('name').value;
 var submit=document.getElementById('submit_btn');
 submit.onclick = function() {
-    
+    var names = ['name1','name2','name3'];
+    var list = '';
+    for(var i=0;i<names.length;i++){
+        list += "<li>" + names[i] + "</li>";
+    }
+    var ui = documnet.getElementById('');
 };

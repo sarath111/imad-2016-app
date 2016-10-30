41517a8fa71be55fdbcb1f5c88b181af97ef5d3a
sarath111 <sarathmohan111@gmail.com>
2016-10-30 12:32:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,6 +20,33 @@ var articleThree = {
             </p>`
 };
 
+var htmlTemplate = 
+`<html>
+    <head>
+        <title>
+            ${title};
+        </title>
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div>
+            <a href ="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading};
+        </h3>
+        <div>
+            ${date};
+        </div>
+        <div>
+            ${content};
+        </div>
+    </body>
+    
+</html>`
+;
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });

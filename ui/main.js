426c64cb2b2d54a9dc1af95d0612dcaaa7c06529
sarath111 <sarathmohan111@gmail.com>
2016-10-30 13:09:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,28 @@
-console.log('Loaded!');
-alert("Hello");
-var txtfield=document.getElementById("id1");
-txtfield.innerHTML="Hello";
+//console.log('Loaded!');
+//alert("Hello");
+//var txtfield=document.getElementById("id1");
+//txtfield.innerHTML="Hello";
 //move the img element
-var img=document.getElementById('img1');
-var marginleft=1;
-img.onclick= function() {
-  alert("clicked on img");
-  setInterval(moveright,50);
+//var img=document.getElementById('img1');
+//var marginleft=1;
+//img.onclick= function() {
+//  alert("clicked on img");
+//  setInterval(moveright,50);
+//};
+//function moveright() {
+//   marginleft=marginleft+1; 
+//  img.style.marginLeft=marginleft+"px";
+//}
+
+var button=document.getElementById('counter');
+button.onclick = function () {
+    //Make a req to counter endpoint
+    
+    
+    //Capture the response and store it in a variable
+    
+    //Rendering the variable in the correct span
+    
+    
+    
 };
-function moveright() {
-   marginleft=marginleft+1; 
-   img.style.marginLeft=marginleft+"px";
-}

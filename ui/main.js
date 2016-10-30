1de661283cbac0ba66b2f337997e40a2b45286d4
sarath111 <sarathmohan111@gmail.com>
2016-10-30 18:33:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -52,6 +52,8 @@ submit.onclick = function() {
             if(request.status == 200){
                 //successfull completion of req
                  //var names = ['name1','name2','name3'];
+                 var names = request.responseText;
+                 names=JSON.parse(names);
                  var list = '';
                  for(var i=0;i<names.length;i++){
                      list += "<li>" + names[i] + "</li>";
@@ -64,7 +66,7 @@ submit.onclick = function() {
     };
     
     //Make a request to counter endpoint
-    request.open('GET','http://sarath111.imad.hasura-app.io/submit-name?name=sarath',true);
+    request.open('GET','http://sarath111.imad.hasura-app.io/submit-name?name='+nameText,true);
     request.send(null);
     
    
